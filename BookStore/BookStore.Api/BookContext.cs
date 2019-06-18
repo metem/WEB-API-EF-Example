6a9632a37d2feb20b968cac1956871c7e92e2faf
Mateusz Ucher <mateusz.ucher@gmail.com>
2019-06-18 22:56:39 +0200

    draft

---
+++ BookStore/BookStore.Api/BookContext.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Api
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BookStore/BookStore.Api/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore.Api.Models
+{
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
